120e3466bbe35f8b675c8ec89715a076d59ab2c5
“baozhigang” <646637969@qq.com>
2022-04-10 13:41:28 +0800

    guzzle 异步请求

--- frontend/controllers/GuzzleController.php
+++ frontend/controllers/GuzzleController.php
@@ -27,10 +27,14 @@ class GuzzleController extends Controller
     }
 
     /**
-     * 这个异步为什么没有生效？
-     * 还是没有拿到异步请求返回的值。
-     * 只有调用 wait 方法同步之后，才能拿到返回值，
-     * 那么怎么使用异步的返回值？
+     * 异步处理，还没处理完，客户端拿不到任何结果，
+     * then 是在回调里打印返回结果，
+     * 如果使用了异步，场景就不是实时的返回给客户端结果，
+     * 而是拿到返回结果，在回调里面操作数据库，或者其他下一步操作
+     * 这是异步和同步的区别，实时返回直接用同步就好了
+     * 不要使用wait调用，那就主动阻塞了
+     *
+     * 重点是了解异步的流程，以及什么时候用异步，什么时候用同步
      */
     public function actionIndex2()
     {
@@ -39,6 +43,7 @@ class GuzzleController extends Controller
 
         $promise->then(
             function (ResponseInterface $res) {
+                // 修改数据库，或者其他操作
                 echo $res->getStatusCode() . "\n";
                 echo $res->getBody()->getContents();
             },
@@ -48,8 +53,7 @@ class GuzzleController extends Controller
         );
 
         // echo $promise->getState();
-
-        $promise->wait();
+        // $promise->wait();
     }
 
     public function actionP1()
